71e776e888c23f9daee33e5acb6f432cfe6bc7ae
Dmytro Mantula <dmantula@gmail.com>
2013-11-18 17:14:17 +0200

    Problem #16

---
+++ src/problem16/Problem16.scala
@@ -0,0 +1,18 @@
+package problem16
+
+/**
+ * 2^^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ *
+ * What is the sum of the digits of the number 2^^1000?
+ */
+object Problem16 extends App {
+  // Power of n as BigInt
+  private def bigPow(n: Int, pow: Int) = BigInt(n).pow(pow)
+
+  // Sum of digits of BigInt
+  private def sumDigits(b: BigInt) = b.toString.toArray.map{c:Char => c - '0'.toInt}.sum
+
+  def calc(pow: Int) = sumDigits(bigPow(2, pow))
+
+  println(calc(1000))
+}
